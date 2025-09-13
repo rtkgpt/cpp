998b905860be875fe660bf1ffaca04ab1c2ccc97
Ritik Gupta <ritikgupta.mail@gmail.com>
2025-09-13 13:04:12 +0530

    Update 02pushBack.cpp

--- LinkedList/02pushBack.cpp
+++ LinkedList/02pushBack.cpp
@@ -21,18 +21,8 @@ public:
         head = tail = NULL;
     }
 
-    void push_front(int val) {
-        Node* newNode = new Node(val);
-
-        if(head == NULL) {
-            head = tail = newNode;
-        } else {
-            newNode->next = head;
-            head = newNode;
-        }
-    }
-
     void push_back(int val) {
+        // this function adds a node at the end of linked list
         Node* newNode = new Node(val);
 
         if(head == NULL) {
@@ -44,6 +34,7 @@ public:
     }
 
     void print() {
+        // function prints linkedlist
         Node* temp = head;
 
         while(temp != NULL) {
@@ -55,12 +46,12 @@ public:
 
 int main() {
     LinkedList ll;
-    ll.push_front(1);
-    ll.push_front(2);
-    ll.push_front(3);
-    ll.push_back(4);
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
 
     ll.print();
 
     return 0;
+
 }
